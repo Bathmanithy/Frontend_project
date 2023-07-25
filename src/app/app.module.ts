492b46e8d64a2111895ab35503677e15da094c4b
Bathmanithy <99311433+Bathmanithy@users.noreply.github.com>
2023-07-25 22:59:40 +0530

    iamneo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,DragDropModule,
+    BrowserAnimationsModule,MatToolbarModule,MatIconModule,
+    MatButtonModule,MatDialogModule,MatFormFieldModule,MatInputModule,
+    ReactiveFormsModule,FormsModule,HttpClientModule,MatCardModule,MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
